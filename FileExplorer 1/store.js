90c9947389743fd4f302b80fcf097a44aa4042be
AnujNagwekar <84332237+AnujNagwekar@users.noreply.github.com>
2023-08-18 12:03:10 +0530

    Add files via upload

---
+++ FileExplorer 1/store.js
@@ -0,0 +1,14 @@
+// refernce of state
+
+let data = [
+    {
+        type:"folder",
+        name:"Root",
+        id:"main_section",
+        level:0,
+        children:[
+            
+        ]
+    }
+]
+
